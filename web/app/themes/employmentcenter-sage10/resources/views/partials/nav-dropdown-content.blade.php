2b983b41d0b9a694ad0d177470e7069f1c2d103a
Kenny Eliason <kenny@neonbrand.com>
2021-03-16 00:04:00 -0700

    menu fix

--- web/app/themes/employmentcenter-sage10/resources/views/partials/nav-dropdown-content.blade.php
+++ web/app/themes/employmentcenter-sage10/resources/views/partials/nav-dropdown-content.blade.php
@@ -8,9 +8,9 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"></path>
                 </svg> --}}
                 <div class="">
-                    <p class="text-base font-medium text-gray-900 pb-0">
+                    <span class="block text-base font-medium text-gray-900 pb-0">
                         {{ $child->label }}
-                    </p>
+                    </span>
                     {{-- <p class="mt-1 text-sm text-gray-500">
                         Get all of your questions answered in our forums or contact support.
                     </p> --}}
